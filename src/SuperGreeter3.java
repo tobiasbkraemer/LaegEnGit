b913ddb2d4b14c0b00b0ec793e496cf8032b517a
Tobias <tobi4533@stud.kea.dk>
2022-09-09 13:57:32 +0200

    added smallTalk

--- src/SuperGreeter3.java
+++ src/SuperGreeter3.java
@@ -9,12 +9,18 @@ public class SuperGreeter3 {
     public void greet () {
         String navn = keyboard.nextLine();
         System.out.println("Hej " + navn+ "!");
+    }
 
+    public void smallTalk () {
+        System.out.println("Hvor gammel er du?");
+        int alder = keyboard.nextInt();
+        System.out.println("Fedt! "+alder+" er gammelt nok til mig ;)");
     }
 
     public static void main(String[] args) {
         SuperGreeter3 sg = new SuperGreeter3();
         sg.start();
         sg.greet();
+        sg.smallTalk();
     }
 }
