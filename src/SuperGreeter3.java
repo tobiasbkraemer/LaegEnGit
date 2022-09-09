545cac900f36914c93ad3a1c177a6d23bd68d8fd
Tobias <tobi4533@stud.kea.dk>
2022-09-09 13:51:47 +0200

    added greet

--- src/SuperGreeter3.java
+++ src/SuperGreeter3.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class SuperGreeter3 {
+    Scanner keyboard = new Scanner(System.in);
     public void start () {
-        System.out.println("Det virker!");
+        System.out.println("Hej! Hvad hedder du?");
+    }
+
+    public void greet () {
+        String navn = keyboard.nextLine();
+        System.out.println("Hej " + navn+ "!");
+
     }
 
     public static void main(String[] args) {
         SuperGreeter3 sg = new SuperGreeter3();
         sg.start();
-        
+        sg.greet();
     }
 }
