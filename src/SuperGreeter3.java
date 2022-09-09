5e342eaeb0d9015347b4bf80574cc21aaa258ef9
Tobias <tobi4533@stud.kea.dk>
2022-09-09 13:36:16 +0200

    Initial

---
+++ src/SuperGreeter3.java
@@ -0,0 +1,11 @@
+public class SuperGreeter3 {
+    public void start () {
+        System.out.println("Det virker!");
+    }
+
+    public static void main(String[] args) {
+        SuperGreeter3 sg = new SuperGreeter3();
+        sg.start();
+        
+    }
+}
